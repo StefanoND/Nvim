df8fd085001bdd87583b225c840b3ad471d570ce
Stefano Dobbin <sndobbin@gmail.com>
2024-01-03 16:50:57 +0000

    Omnisharp update
    Added Windows Support
    Added Mono support

--- lua/stf/config/lsp/omnisharp/omnisharp.lua
+++ lua/stf/config/lsp/omnisharp/omnisharp.lua
@@ -1,4 +1,7 @@
+local shouldUseMono = true
+
 local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 local cmp_lsp = require("cmp_nvim_lsp")
 local capabilities = cmp_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -14,7 +17,7 @@ local omni_on_attach = function(client, bufnr)
 	-- Omnicompletion
 	vim.api.buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	local opts = { noremap = true, silent = true }
+	local opts = { buffer = bufnr, remap = false, silent = true }
 	vim.api.buf_set_keymap(bufnr, "n", "<leader>vws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", opts)
 	vim.api.buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.buf_set_keymap(
@@ -40,11 +43,12 @@ local omni_on_attach = function(client, bufnr)
 		bufnr,
 		{ "n", "x" },
 		"<leader>f",
-		"<cmd>lua vim.lsp.buf.format({ async = false, timeout_ms = 10000 })<CR>",
+		"<cmd>lua vim.lsp.buf.format({ async = true, timeout_ms = 10000 })<CR>",
 		opts
 	)
 	print("hello omnisharp")
 end
+-- end
 
 -- Omnisharp/C#/Unity
 local pid = vim.fn.getpid()
@@ -61,12 +65,31 @@ if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win16") =
 			enableProfileLoading = false,
 		},
 	})
-	omnisharp_bin = os.getenv("UserProfile") .. "/AppData/Local/nvim/omnisharp-win-x64_1.39.8/OmniSharp.exe"
+	if shouldUseMono == true then
+		omnisharp_bin = os.getenv("UserProfile") .. "/AppData/Local/nvim/omnisharp-mono_1.39.8/OmniSharp.exe"
+		-- omnisharp_bin = os.getenv("UserProfile") .. "/AppData/Local/nvim/omnisharp-mono_1.39.11/OmniSharp.exe"
+	else
+		omnisharp_bin = os.getenv("UserProfile") .. "/AppData/Local/nvim/omnisharp-win-x64_1.39.8/OmniSharp.exe"
+	end
 else -- I don't own/use a Mac, will update when/if I do
 	omnisharp_bin = os.getenv("HOME") .. "/.config/nvim/omnisharp-linux-x64_1.39.8/run"
 end
 
 local omnisharp_config = {
+	use_mono = shouldUseMono,
+	default_config = {
+		filetypes = { "cs", "vb" },
+		root_dir = function(fname)
+			return util.root_pattern("*.sln")(fname) or util.root_pattern("*.csproj")(fname)
+		end,
+		on_new_config = function(new_config, new_root_dir)
+			if new_root_dir then
+				table.insert(new_config.cmd, "-s")
+				table.insert(new_config.cmd, new_root_dir)
+			end
+		end,
+		init_options = {},
+	},
 	enable_editorconfig_support = true,
 	enable_roslyn_analyzers = true,
 	enable_import_completion = true,
@@ -77,13 +100,7 @@ local omnisharp_config = {
 	flags = {
 		debounce_text_changes = 150,
 	},
-	-- on_init = function(client)
-	-- 	client.server_capabilities.semanticTokensProvider = nil
-	-- end,
-	on_attach = function(client, bufnr)
-		omni_on_attach(client, bufnr)
-		print("hello omnisharp")
-	end,
+	on_attach = omni_on_attach,
 	capabilities = capabilities,
 	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
 }

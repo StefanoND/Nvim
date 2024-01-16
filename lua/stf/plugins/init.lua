7e78b7166fc1dcd72628880e1c855c108846a3fb
Stefano Dobbin <sndobbin@gmail.com>
2024-01-16 16:45:15 +0000

    Update

--- lua/stf/plugins/init.lua
+++ lua/stf/plugins/init.lua
@@ -325,10 +325,9 @@ return {
   },
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     config = function()
-      require("ibl").setup({
-        scope = { enabled = false },
-      })
+      require("stf.config.indent")
     end,
   },
   {
